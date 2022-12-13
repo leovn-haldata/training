b1df6efb5d094f9fdabbcc3e7f2ffe0222309c48
rcvnkay <nguyen.dang.khoa.rcvn2012@gmail.com>
2022-12-13 10:27:29 +0700

    - Fix design screen login + register cho đúng yêu cầu #2

---
+++ app/Http/Requests/Auth/RegisterRequest.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Http\Requests\Auth;
+
+use App\Models\User;
+use Illuminate\Auth\Events\Registered;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\Exceptions\HttpResponseException;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Validation\Rules;
+
+class RegisterRequest extends FormRequest
+{
+    protected $stopOnFirstFailure = false;
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'name' => ['required', 'string', 'max:255'],
+            'email' => ['required', 'string', 'email', 'max:255', 'unique:users','regex:/(.+)@(.+)\.(.+)/i'],
+            'password' => ['required', 'confirmed','required_with:password_confirmation|same:password_confirmation', Rules\Password::defaults()],
+        ];
+    }
+
+    public function regist() {
+
+        $user = User::create([
+            'name' => $this->input('name'),
+            'email' => $this->input('email'),
+            'password' => Hash::make($this->input('password')),
+            'is_active' => 1,
+            'is_delete' => 0,
+            'group_role' => 0,
+        ]);
+
+        event(new Registered($user));
+        Auth::login($user);
+    }
+}
